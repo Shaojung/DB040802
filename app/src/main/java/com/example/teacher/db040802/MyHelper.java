7c3953e9cdb12e4a9a86211cb8c5e09c35bd8816
Shaojung <shaojung@gmail.com>
2017-04-08 15:50:49 +0800

    onUpgrade test

--- app/src/main/java/com/example/teacher/db040802/MyHelper.java
+++ app/src/main/java/com/example/teacher/db040802/MyHelper.java
@@ -9,7 +9,7 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 
 public class MyHelper extends SQLiteOpenHelper {
-    static final int version = 1;
+    static final int version = 2;
     static final String dbname = "student.sqlite";
     public MyHelper(Context context) {
         super(context, dbname, null, version);
@@ -17,11 +17,14 @@ public class MyHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("CREATE  TABLE \"phone1\" (\"ID\" INTEGER PRIMARY KEY  NOT NULL , \"Name\" VARCHAR, \"Tel\" VARCHAR, \"Addr\" VARCHAR)");
+        db.execSQL("CREATE TABLE \"phone1\" (\"ID\" INTEGER PRIMARY KEY  NOT NULL , \"Name\" VARCHAR, \"Tel\" VARCHAR, \"Addr\" VARCHAR, \"Email\" VARCHAR)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
+        if (oldVersion == 1 && newVersion == 2)
+        {
+            db.execSQL("ALTER TABLE \"main\".\"phone1\" ADD COLUMN \"Email\" VARCHAR");
+        }
     }
 }
